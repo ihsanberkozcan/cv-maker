c59a7b83d5b0bb9af54b60dde8896cf95487a6cc
İhsan Berk Özcan <59116996+ihsanberkozcan@users.noreply.github.com>
2023-07-18 22:35:00 +0300

    delete icon update

--- src/components/AddLink.tsx
+++ src/components/AddLink.tsx
@@ -72,7 +72,20 @@ function AddLink() {
             className="bg-indigo-300 rounded text-white mb-3 p-3"
             onClick={() => deleteWebsiteLink(i)}
           >
-            x
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
           </button>
         </li>
       );

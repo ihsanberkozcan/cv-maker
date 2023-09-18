63b1e45f48b5d5263374cfb4a0d8288aafb97a99
İhsan Berk Özcan <59116996+ihsanberkozcan@users.noreply.github.com>
2023-09-18 23:07:32 +0300

    icon

--- src/components/ModernResume.tsx
+++ src/components/ModernResume.tsx
@@ -7,6 +7,8 @@ import {
   BlobProvider,
   PDFDownloadLink,
   Font,
+  Svg,
+  Path,
 } from "@react-pdf/renderer";
 
 import { pdfjs, Document as ReactDocument, Page as ReactPage } from "react-pdf";
@@ -18,7 +20,6 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
   import.meta.url
 ).toString();
-
 
 import { useTranslation } from "react-i18next";
 
@@ -105,6 +106,9 @@ const styles = StyleSheet.create({
   description: {
     fontSize: 12,
   },
+  sectionHeader: {
+    flexDirection: "row",
+  },
 });
 
 export const Content = ({
@@ -169,7 +173,18 @@ export const Content = ({
 
           {experience.length ? (
             <View style={styles.section}>
-              <Text style={styles.subtitle}>{t("Experience")}</Text>
+              <View style={styles.sectionHeader}>
+                <Svg style={{ width: 30, height: 30 }} stroke-width="1.5">
+                  <Path
+                    style={{ stroke: "blue" }}
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
+                  />
+                </Svg>
+
+                <Text style={styles.subtitle}>{t("Experience")}</Text>
+              </View>
               <View style={styles.line} />
               {experience?.map((myExperience: experienceType) => (
                 <View style={styles.subSection}>
@@ -191,7 +206,19 @@ export const Content = ({
           ) : null}
           {education.length ? (
             <View style={styles.section}>
-              <Text style={styles.subtitle}>{t("Education")}</Text>
+              <View style={styles.sectionHeader}>
+                <Svg style={{ width: 30, height: 30 }} stroke-width="1.5">
+                  <Path
+                    style={{ stroke: "blue" }}
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"
+                  />
+                </Svg>
+
+                <Text style={styles.subtitle}>{t("Education")}</Text>
+              </View>
+
               <View style={styles.line} />
               {education.map((myEducation: educationType) => (
                 <View style={styles.subSection}>
@@ -216,7 +243,18 @@ export const Content = ({
           ) : null}
           {project.length ? (
             <View style={styles.section}>
-              <Text style={styles.subtitle}>{t("Projects")}</Text>
+              <View style={styles.sectionHeader}>
+                <Svg style={{ width: 30, height: 30 }} stroke-width="1.5">
+                  <Path
+                    style={{ stroke: "blue" }}
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
+                  />
+                </Svg>
+
+                <Text style={styles.subtitle}>{t("Projects")}</Text>
+              </View>
               <View style={styles.line} />
               {project.map((myProject: projectType) => (
                 <View style={styles.subSection}>
@@ -233,7 +271,18 @@ export const Content = ({
           ) : null}
           {certification.length ? (
             <View style={styles.section}>
-              <Text style={styles.subtitle}>{t("Certifications")}</Text>
+              <View style={styles.sectionHeader}>
+                <Svg style={{ width: 30, height: 30 }} stroke-width="1.5">
+                  <Path
+                    style={{ stroke: "blue" }}
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                  />
+                </Svg>
+
+                <Text style={styles.subtitle}>{t("Certifications")}</Text>
+              </View>
               <View style={styles.line} />
               {certification.map((myCertification: certificationType) => (
                 <View style={styles.subSection}>
@@ -254,7 +303,18 @@ export const Content = ({
           ) : null}
           {award.length ? (
             <View style={styles.section}>
+              <View style={styles.sectionHeader}>
+                <Svg style={{ width: 30, height: 30 }} stroke-width="1.5">
+                  <Path
+                    style={{ stroke: "blue" }}
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M16.5 18.75h-9m9 0a3 3 0 013 3h-15a3 3 0 013-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 01-.982-3.172M9.497 14.25a7.454 7.454 0 00.981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 007.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 002.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 012.916.52 6.003 6.003 0 01-5.395 4.972m0 0a6.726 6.726 0 01-2.749 1.35m0 0a6.772 6.772 0 01-3.044 0"
+                  />
+                </Svg>
+
               <Text style={styles.subtitle}>{t("Awards")}</Text>
+              </View>
               <View style={styles.line} />
               {award.map((myAward: awardType) => (
                 <View style={styles.subSection}>
@@ -327,7 +387,7 @@ export const ModernResume = () => {
             />
           }
         >
-          {({ url}) => {
+          {({ url }) => {
             return (
               <div>
                 <ReactDocument file={url} onLoadSuccess={onDocumentLoadSuccess}>

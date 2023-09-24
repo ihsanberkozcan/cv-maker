5ebf4da47ebed931afe2c270768873e5699270e7
İhsan Berk Özcan <59116996+ihsanberkozcan@users.noreply.github.com>
2023-09-24 23:51:45 +0300

    Links Icon

--- src/components/ModernResume.tsx
+++ src/components/ModernResume.tsx
@@ -9,6 +9,7 @@ import {
   Font,
   Svg,
   Path,
+  Link,
 } from "@react-pdf/renderer";
 
 import { pdfjs, Document as ReactDocument, Page as ReactPage } from "react-pdf";
@@ -54,7 +55,7 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
-    marginTop:"-30px"
+    marginTop: "-30px",
   },
   content: {
     paddingHorizontal: "40",
@@ -72,7 +73,13 @@ const styles = StyleSheet.create({
     width: "100%",
   },
   links: {
+    flexWrap: "wrap",
     flexDirection: "row",
+    maxWidth: "100%",
+  },
+  linkText: {
+    flexDirection: "row",
+    alignItems: "center",
   },
   jobTitle: {
     fontSize: 18,
@@ -93,6 +100,11 @@ const styles = StyleSheet.create({
     fontSize: 13,
   },
   text: {
+    fontSize: 12,
+    marginBottom: 5,
+  },
+  link: {
+    color: "#fff",
     fontSize: 12,
     marginBottom: 5,
   },
@@ -151,9 +163,48 @@ export const Content = ({
             <View style={styles.links}>
               {links?.map((mylink: linksType, index: number) => (
                 <>
-                  <Text style={styles.text}>{mylink.websiteName} :&nbsp;</Text>
-                  <Text style={styles.text}>{mylink.link}</Text>
+                  <View style={styles.linkText}>
+                    {mylink.websiteName == "GitHub" ? (
+                      <Svg style={{ width: 20, height: 20 }}>
+                        <Path
+                          style={{ fill: "#fff" }}
+                          d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+                        />
+                      </Svg>
+                    ) : mylink.websiteName == "GitLab" ? (
+                      <Svg style={{ width: 20, height: 20 }}>
+                        <Path
+                          style={{ fill: "#fff" }}
+                          d="m15.734 6.1-.022-.058L13.534.358a.568.568 0 0 0-.563-.356.583.583 0 0 0-.328.122.582.582 0 0 0-.193.294l-1.47 4.499H5.025l-1.47-4.5A.572.572 0 0 0 2.47.358L.289 6.04l-.022.057A4.044 4.044 0 0 0 1.61 10.77l.007.006.02.014 3.318 2.485 1.64 1.242 1 .755a.673.673 0 0 0 .814 0l1-.755 1.64-1.242 3.338-2.5.009-.007a4.046 4.046 0 0 0 1.34-4.668Z"
+                        />
+                      </Svg>
+                    ) : mylink.websiteName == "LinkedIn" ? (
+                      <Svg style={{ width: 20, height: 20 }}>
+                        <Path
+                          style={{ fill: "#fff" }}
+                          d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"
+                        />
+                      </Svg>
+                    ) : mylink.websiteName == "Medium" ? (
+                      <Svg style={{ width: 20, height: 20 }}>
+                        <Path
+                          style={{ fill: "#fff" }}
+                          d="M9.025 8c0 2.485-2.02 4.5-4.513 4.5A4.506 4.506 0 0 1 0 8c0-2.486 2.02-4.5 4.512-4.5A4.506 4.506 0 0 1 9.025 8zm4.95 0c0 2.34-1.01 4.236-2.256 4.236-1.246 0-2.256-1.897-2.256-4.236 0-2.34 1.01-4.236 2.256-4.236 1.246 0 2.256 1.897 2.256 4.236zM16 8c0 2.096-.355 3.795-.794 3.795-.438 0-.793-1.7-.793-3.795 0-2.096.355-3.795.794-3.795.438 0 .793 1.699.793 3.795z"
+                        />
+                      </Svg>
+                    ) : mylink.websiteName == "Website" ? (
+                      <Svg style={{ width: 20, height: 20 }}>
+                        <Path
+                          style={{ fill: "#fff" }}
+                          d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103z"
+                        />
+                      </Svg>
+                    ) : null}
 
+                    <Link src={mylink.link} style={styles.link}>
+                      {mylink.link}
+                    </Link>
+                  </View>
                   {index !== links.length - 1 ? (
                     <Text style={styles.text}>&nbsp;|&nbsp;</Text>
                   ) : (
